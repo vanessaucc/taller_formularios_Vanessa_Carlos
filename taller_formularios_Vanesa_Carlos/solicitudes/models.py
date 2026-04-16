4dbc7a8c92f2abb8c72ce2828d4cfec8d2652c0e
C-David-Burbano-C <david.burbano@simerelectronics.com>
2026-04-16 09:19:05 -0500

    migrations, templates

--- taller_formularios_Vanesa_Carlos/solicitudes/models.py
+++ taller_formularios_Vanesa_Carlos/solicitudes/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Solicitud(models.Model):
+    ACADEMICA = 'academica'
+    ADMINISTRATIVA = 'administrativa'
+    TECNICA = 'tecnica'
+    OTRA = 'otra'
+
+    TIPOS_SOLICITUD = [
+        (ACADEMICA, 'Académica'),
+        (ADMINISTRATIVA, 'Administrativa'),
+        (TECNICA, 'Técnica'),
+        (OTRA, 'Otra'),
+    ]
+
+    nombre_solicitante = models.CharField(max_length=150)
+    documento_identidad = models.CharField(max_length=30)
+    correo_electronico = models.EmailField()
+    telefono_contacto = models.PositiveBigIntegerField()
+    tipo_solicitud = models.CharField(max_length=20, choices=TIPOS_SOLICITUD)
+    asunto = models.CharField(max_length=150)
+    descripcion_detallada = models.TextField()
+    fecha_solicitud = models.DateField()
+    archivo_adjunto = models.FileField(upload_to='solicitudes/', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.asunto} - {self.nombre_solicitante}'
