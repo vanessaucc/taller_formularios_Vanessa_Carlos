d1a36f392a53280e51bcd6836160a6fd721c31c2
vanessaucc <julieth.mena@campusucc.edu.co>
2026-04-16 09:18:25 -0500

    asistencia

--- taller_formularios_Vanesa_Carlos/asistencia/views.py
+++ taller_formularios_Vanesa_Carlos/asistencia/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import AsistenciaForm
+
+
+def registrar_asistencia(request):
+    if request.method == 'POST':
+        form = AsistenciaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('asistencia_confirmacion')
+    else:
+        form = AsistenciaForm()
+
+    return render(request, 'asistencia/formulario.html', {'form': form})
+
+
+def confirmacion_asistencia(request):
+    return render(request, 'asistencia/confirmacion.html')
